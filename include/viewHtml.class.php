a9a7f6c7b144119d8a874156c753db687e0e67e3
godcheese <godcheese@outlook.com>
2016-01-27 19:52:40 +0800

    Update viewHtml.class.php

--- include/viewHtml.class.php
+++ include/viewHtml.class.php
@@ -12,34 +12,111 @@
  */
 
 /**
- * Class viewHtml
- * 路由视图类
+ * Class htmlView
+ * 框架自带 html视图代码输出类
  *
  */
 
-class viewHtml
+class htmlView
 {
     /**
-     * @param $str
+     * @param string $str 标签末换行输出
      */
-    static public function print_line($str){
+    static public function print_l($str)
+    {
         echo $str.'
-        ';
+';
     }
-
 
     /**
-     * html 标签
-     * @param $lang 页面语言 zh_CN
+     * @param int $oT 标签输出格式 0=<tag attr="attrV"> 1=<tag attr="attrV"></tag>
+     * @param array $in
      */
-    static public function html_start($lang = 'zh_CN'){
-        $str='
-<!DOCTYPE html>
-<html lang="'.$lang.'">';
-        echo $str;
+    static private function format_tag($oT = 0, $in = array('tag' => '', 'attr' => array(), 'content' => ''))
+    {
+        if (array_key_exists('attr', $in)&& !empty($in['attr'])) {//检测$in数组内是否存在 attr key
+
+            //有attr key 遍历格式输出数组key及值
+            $vv = '';
+            foreach ($in['attr'] as $key => $value) {
+                $vv = $vv . ' ' . $key . '="' . $value . '"';
+            }
+            switch ($oT) {
+                case 0:
+                    return '<' . $in['tag'] . $vv . '>';
+                    break;
+                case 1:
+                    if (array_key_exists('content', $in)) {//检测$in数组内是否存在 content key
+                        //有content key
+                        return '<' . $in['tag'] . $vv . '>' . $in['content'] . '</' . $in['tag'] . '>';
+                    } else {
+                        //无content key
+                        $in['content'] = '';
+                        return '<' . $in['tag'] . $vv . '>' . $in['content'] . '</' . $in['tag'] . '>';
+                    }
+                    break;
+                default:
+                    return '<' . $in['tag'] . $vv . '>';
+                    break;
+            }
+        }else{
+            $vv='';
+
+            //判断输出格式
+            switch ($oT) {
+                case 0:
+                    return '<' . $in['tag'] . $vv . '>';
+                    break;
+                case 1:
+                    if (array_key_exists('content', $in)) {//检测$in数组内是否存在 content key
+                        //有content key
+                        return '<' . $in['tag'] . $vv . '>' . $in['content'] . '</' . $in['tag'] . '>';
+                    } else {
+                        //无content key
+                        $in['content'] = '';
+                        return '<' . $in['tag'] . $vv . '>' . $in['content'] . '</' . $in['tag'] . '>';
+                    }
+                    break;
+                default:
+                    return '<' . $in['tag'] . $vv . '>';
+                    break;
+            }
+
+        }
     }
-    static public function html_end(){
-        $str='</html>';
+
+    /**
+     * @param $str
+     * @return string
+     * 在标签末尾插入/
+     */
+    static private function insertBackSlant($str)
+    {
+        $str = rtrim($str, '>');
+        $str = $str . '/>';
+        return $str;
+    }
+
+    /**
+     * @param array $attr 标签属性
+     * html 标签
+     */
+    static public function html_start($attr = array())
+    {
+        if (count($attr) != null) {
+            $str = self::format_tag(1, array('tag' => 'html', 'attr' => $attr));
+            $str = self::print_l('<!DOCTYPE html>') . $str;
+            $str = rtrim($str, '</html>') . '>';
+            echo $str;
+        } else {
+            $str = '<!DOCTYPE html>
+<html lang="zh_CN">';
+            echo $str;
+        }
+    }
+    static public function html_end()
+    {
+        $str = '</html>';
         echo $str;
     }
 
@@ -47,51 +124,31 @@ class viewHtml
     /**
      * head 标签
      */
-    static public function head_start(){
-        $str='<head>';
+    static public function head_start()
+    {
+        $str = '<head>';
         echo $str;
     }
-    static public function head_end(){
-        $str='</head>';
+    static public function head_end()
+    {
+        $str = '</head>';
         echo $str;
     }
 
     /**
-     * @param int $outputType   0=<tag attr=v>;1=<tag attr=v></tag>
-     * @param array $input  array('tag'=>'','attr'=>array(),'content'=>'')
+     * @param int $outputType 0=<tag attr=v>;1=<tag attr=v></tag>
+     * @param array $input array('tag'=>'','attr'=>array(),'content'=>'')
      *
      */
-    static public function tag_format_output_($outputType=0,$input=array('tag'=>'','attr'=>array(),'content'=>'')){
+    static public function tag_format_output($outputType = 0, $input = array('tag' => '', 'attr' => array(), 'content' => ''))
+    {
 
         /**
          * 格式化标签输出函数
-         * @param int $oT   0=<tag attr=v>;1=<tag attr=v></tag>
+         * @param int $oT 0=<tag attr=v>;1=<tag attr=v></tag>
          * @param array $in array('tag'=>'','attr'=>array(),'content'=>'')
          * @return string
          */
-        function format_tag($oT=0,$in=array('tag'=>'','attr'=>array(),'content'=>''))
-        {
-            $vv='';
-            foreach ($in['attr'] as $key => $value) {
-                $vv = $vv . ' ' . $key . '="' . $value . '"';
-            }
-            switch ($oT){
-                case 0:
-                    echo '<' . $in['tag'] . $vv . '>';
-                    break;
-                case 1:
-                    if(!array_key_exists('content',$in)){
-                        $in['content']='';
-                        echo '<'.$in['tag'].$vv.'>'.$in['content'].'</'.$in['tag'].'>';
-                    }else {
-                        echo '<' . $in['tag'] . $vv . '>' . $in['content'] . '</' . $in['tag'] . '>';
-                    }
-                    break;
-                default:
-                    echo '<' . $in['tag'] . $vv . '>';
-                    break;
-            }
-        }
 
 
         /**
@@ -99,65 +156,58 @@ class viewHtml
          * @param $str 要添加反斜杠的字符串
          * @return string
          */
-        function insertBackSlant($str){
-            $str=rtrim($str,'>');
-            $str=$str.'/>';
-            return $str;
-        }
 
-        switch ($input['tag']){
+
+        switch ($input['tag']) {
             case 'meta':    //<meta attr="value" >
-                $res=format_tag(0,$input);
+                $res = self::format_tag($outputType, $input);
                 echo $res;
                 break;
             case 'link':    //<link attr="value" >
-                $res=format_tag(0,$input);
+                $res = self::format_tag($outputType, $input);
                 echo $res;
                 break;
             case 'title':   //<title></title>
-                $res=format_tag(1,$input);
+                $res = self::format_tag(1, $input);
                 echo $res;
                 break;
             case 'script':  //<script attr="value" >...</script>
-                $res=format_tag($outputType,$input);
+                $res = self::format_tag($outputType, $input);
                 echo $res;
                 break;
             case 'style':   //<style type="text/stylesheet">...</style>
-                $res=format_tag(1,$input);
+                $res = self::format_tag(1, $input);
                 echo $res;
                 break;
 
             default:
-                $res=format_tag(0,$input);
+                $res = self::format_tag($outputType, $input);
                 echo $res;
                 break;
         }
 
-
-
-
     }
 
     /**
-     * body 标签
-     * @param $styleclass body标签引用的样式类或
+     * @param array $attr body 属性参数
      */
-    static public function body_start($styleclass=null){
-
-        if(!empty($styleclass)){
-            $str='
-        <body '.$styleclass.'>';
+    static public function body_start($attr=array())
+    {
+        if (count($attr) != null) {
+            $str = self::format_tag(1, array('tag' => 'body', 'attr' => $attr));
+            $str = rtrim($str, '</body>') . '>';
             echo $str;
-        }else{
-            $str='<body>';
+        } else {
+            $str = '<body>';
             echo $str;
         }
     }
-
     static public function body_end(){
-        $str='</body>';
+        $str = '</body>';
         echo $str;
+
     }
 
 }
+
 ?>
