cbc1b5c44dc4f66860afaa29bf3c6e005507a4a7
godcheese <godcheese@outlook.com>
2016-01-28 16:57:27 +0800

    Update system.controller.php

--- include/system.controller.php
+++ include/system.controller.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @project :   GiWebFrame
- * @version :   v1.0 alpha
+ * @version :   v1.1 alpha
  * @author  :   godcheese
  * @website :   http://www.gioov.com
  * @github  :   https://github.com/godcheese
@@ -18,7 +18,7 @@
  */
 
 //引用html视图代码输出类
-require_once ROOT_PATH . '/include/htmlView.class.php';
+require_once ROOT_PATH . '/include/view.class.php';
 
 class system{
 
@@ -26,24 +26,24 @@ class system{
      * 当只访问控制器时，默认调用此方法。例如：http://www.gioov.com/?c=page 或 http://www.gioov.com/?c=page&m=
      */
     public function init(){
-        htmlView::print_l(htmlView::html_start(array('lang'=>'zh_CN', 'xmlns'=>'http://www.w3.org/1999/xhtml')));
-        htmlView::print_l(htmlView::head_start());
-        htmlView::print_l(htmlView::tag_format_output(0,array('tag'=>'meta','attr'=>array('charset'=>'utf-8'))));
+        view::print_l(view::html_start(array('lang'=>'zh_CN', 'xmlns'=>'http://www.w3.org/1999/xhtml')));
+        view::print_l(view::head_start());
+        view::print_l(view::tag_format_output(0,array('tag'=>'meta','attr'=>array('charset'=>'utf-8'))));
 
         //标题 title
         $page_title='默认页面';
-        htmlView::print_l(htmlView::tag_format_output(1,array('tag'=>'title','attr'=>'','content'=>$page_title)));
-        htmlView::print_l(htmlView::head_end());
+        view::print_l(view::tag_format_output(1,array('tag'=>'title','attr'=>'','content'=>$page_title)));
+        view::print_l(view::head_end());
 
         //body 输出
-        htmlView::print_l(htmlView::body_start(array('class'=>'body')));
+        view::print_l(view::body_start(array('class'=>'body')));
 
         $content="默认页面 sample：测试输出！";
-        htmlView::print_l(htmlView::tag_format_output(1,array('tag'=>'p','attr'=>array('class'=>'container'),'content'=>$content)));
+        view::print_l(view::tag_format_output(1,array('tag'=>'p','attr'=>array('class'=>'container'),'content'=>$content)));
 
-        htmlView::print_l(htmlView::body_end());
+        view::print_l(view::body_end());
 
-        htmlView::html_end();
+        view::html_end();
     }
 
     /**
